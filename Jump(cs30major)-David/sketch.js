f60057a1b1d28eec1b39ec62add1fbb0024fe711
TacoPotato <77638119+TacoPotato@users.noreply.github.com>
2021-02-10 18:51:48 -0600

    Started code, Created new class(character).

--- Jump(cs30major)-David/sketch.js
+++ Jump(cs30major)-David/sketch.js
@@ -1,15 +1,47 @@
-// Project Title
-// Your Name
-// Date
+// Jump - CS30 Major-Project
+// David(Zihan) Yang
+// February 10th, 2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let player;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  player = new Character();
 }
 
 function draw() {
   background(220);
+  player.display();
+  player.move();
 }
+
+class Character{                  
+  constructor(){          
+       this.w = ;              
+       this.h = ;              
+       this.x = ;              
+       this.y = height-this.h; 
+       this.dx = ;    
+  }  
+  
+  jump(){                     
+  }
+  
+  move(){                                       
+  }
+  
+  display(){                                       
+    rect(this.x,this.y,this.w,this.h);         
+  }  
+}
+
+function keyPressed() {
+  if (key === 87 || key === 38) {
+    player.jump();
+  }
+}
+
+  
