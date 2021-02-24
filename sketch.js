12c691b126e95ab93e723f8b57c332facbff80ab
TacoPotato <77638119+TacoPotato@users.noreply.github.com>
2021-02-24 14:08:07 -0600

    airObstacles added, images for airObstacles added

--- sketch.js
+++ sketch.js
@@ -5,10 +5,10 @@
 // Project explanation/Nice to have:
 // - describe what you did to take this project "above and beyond"
 
-let player, gObs;
+let player, gObs, aObs;
 let lastAddedObstacle = 0;
 let gObstacles = []; 
-let airObstacles = [];               
+let aObstacles = [];               
 let imgPlayer = [];
 let imgGroundObs = [];
 let imgAirObs = [];       
@@ -31,12 +31,17 @@ function preload(){
     let string2 = "assets/cactus"+j+".png"; 
     imgGroundObs[j]= loadImage(string2);       
   }
+  for (let k=1; k<=2; k++) {           
+    let string3 = "assets/parrot"+k+".png"; 
+    imgGroundObs[k]= loadImage(string3);       
+  }
 }
 
 function setup() {
   createCanvas(800, 400);
   player = new Character();
   gObs = new groundObstacles();
+  // aObs = new airObstacles();
 
 }
 
@@ -49,13 +54,27 @@ function draw() {
     gObstacles.push(new groundObstacles());         
     lastAddedObstacle = millis();              
   }
-  if (frameCount%6===0) {
+  
+  if (frameCount%6 === 0) {
     imageIndex++;
   } 
   for (let i of gObstacles) {      
     i.move();                           
     i.display();     
     if(player.collide(i)){     
+      CLD = 1; 
+      textAlign(CENTER);       
+      textSize(70);             
+      text("Game Over",width/2, height/2);
+      noLoop();   
+    }
+                        
+  }
+  for (let j of aObstacles) {      
+    j.move();                           
+    j.display(); 
+    j.update();    
+    if(player.collide(j)){     
       CLD = 1; 
       textAlign(CENTER);       
       textSize(70);             
@@ -112,8 +131,8 @@ class Character{
     }
   }
 
-  collide(gObs) {
-    return collideRectRect(this.x,this.y-20,this.w,this.h, gObs.x, gObs.y, gObs.w, gObs.h);
+  collide(Obs) {
+    return collideRectRect(this.x,this.y-20,this.w,this.h, Obs.x, Obs.y, Obs.w, Obs.h);
 
   }
 }
@@ -124,7 +143,7 @@ class groundObstacles {
     this.h = 90;
     this.x = width;
     this.y = height - this.h;
-    this.img = imgGroundObs[int(random(1,12))];
+    this.img = imgGroundObs[int(random(1,13))];
     this.w = this.img.width*2.2;
     this.h = this.img.height*2.2;
    
@@ -139,21 +158,37 @@ class groundObstacles {
   }
 }
 
-class airObstacles {
-  constructor() {
-    this.x;
-    this.y;
-    this.dx;
-  }
+// class airObstacles {
+//   constructor() {
+//     this.w = 50;
+//     this.h = 100;
+//     this.x = width;
+//     this.y = height/2;
+//     this.dx = -10;
+//     this.img = imgAirObs[1];
+//     this.w = this.img.width*1.5;
+//     this.h = this.img.height*1.5;
 
-  move() {
-    this.x -= 10;
-  }
+//   }
 
-  display() {
-    rect(this.x,this.y,this.w,this.h);
-  }
-}
+//   move() {
+//     this.x = this.x-this.dx;
+//   }
+
+//   display() {
+//     image(this.img,this.x,this.y,this.w,this.h);
+//   }
+
+//   update() {
+//     if(CLD === 0){                                                                
+//       this.img = imgAirObs[imageIndex];         
+//     }  
+//     else {                                      
+//       this.img = imgPlayer[0];                   
+//     }
+
+//   }
+// }
 
 
 function keyPressed() {
@@ -163,50 +198,17 @@ function keyPressed() {
 }
 
 // function printScore() { 
-//   textAlign(LEFT);      
-//   fill(50);       
-//   textSize(30); 
-//   text("Score: "+ score, 5*width/6, height/9); 
+//  
 // }
 
 
 // function StartScreen() {             
-//   background(236, 240, 241);         
-//   textAlign(CENTER);               
-//   fill(52, 73, 94);                
-//   textSize(100);                     
-//   text("Start", width/2, height/2); 
-
-//   fill(92,167,182);                 
-//   noStroke();                        
-//   rectMode(CENTER);                  
-//   rect(width/2, height-40, 200,60,5); 
-//   fill(236,240,241);               
-//   textSize(30);                  
-//   text("Jump", width/2, height-30);   
+//  
+// 
 // }
 
 // function endScreen() { 
-
-//   background(23, 24, 24,3);   
-//   textAlign(CENTER);        
-//   fill(255, 227, 132);                  
-//   textSize(30);                            
-//   text("Highest Score", width/2, height/10);           
-
-//   fill(230, 180, 80);                      
-//   textSize(30);                          
-//   text("Score", width/2, height/2-110);    
-//   textSize(150);                       
-//   text(score, width/2, height/2+50);       
-
-//   fill(92,167,182);                  
-//   rectMode(CENTER);                
-//   noStroke();                      
-//   rect(width/2, height-40, 200,60,5);   
-//   fill(236,240,241);                 
-//   textSize(30);                   
-//   text("Restart PLay Again", width/2, height-30);    
+// 
 // }
 
 
