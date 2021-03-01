a1e56d58fb3723feb9257c3a0c3de5ad6bb687bd
TacoPotato <77638119+TacoPotato@users.noreply.github.com>
2021-03-01 09:06:13 -0600

    Final Project/edition

--- sketch.js
+++ sketch.js
@@ -1,48 +1,58 @@
-// Jump - CS30 Major-Project
+// Jumping Dino - CS30 Major-Project
 // David(Zihan) Yang
 // February 10th, 2021
 //
-// Project explanation/Nice to have:
-// - describe what you did to take this project "above and beyond"
+// WOW Factor:
+// - Animated objects, character, background, etc
+// - Use/Exploration of millis(), frameCount(), and Classes
+// - Use of multiple ideas learned in CS30 arrays, external files, libraries, object-oriented programming and etc
 
-let player, gObs, aObs;
-let lastAddedObstacle;
-let gObstacles; 
+
+
+
+// global variables created
+let player, gObs, aObs;            // objects variables
+let lastAddedObstacle;             // time for adding new obstacles to game
+let gObstacles;                    // obstacles array(ground and air)
 let aObstacles;               
-let imgPlayer = [];
+let imgPlayer = [];                // array for images used
 let imgGroundObs = [];
 let imgAirObs = []; 
 
-let imgBackground;
-let bcX;
+let imgBackground;                 // background images(clouds, mountain)
+let bcX;                           // background x-position
 let imgFloor;
 let imgClouds;
 
-let menuMusic;
+let menuMusic;                     // music and sound effects
 let bgMusic;
 let jumpSound;
 let clickSound;   
 let gameOverSound;
 
-let CLD;
-let imageIndex;
+let CLD;                           // collision detection variable
+let imageIndex;                    // switch/determine images and animation
 
-let score;                           
+let score;                         // in game variables
 let screen;       
-let startTime; 
+let startTime;                     // score keeping variable
+let colours;
 
-function preload(){                     
-  for (let i=1; i<=5; i++) {           
+
+
+// loading assets such as images, sound effects, background music
+function preload(){                      
+  for (let i=1; i<=5; i++) {                  // load images for dino
     let string1 = "assets/dino"+i+".png"; 
-    imgPlayer[i]= loadImage(string1);       
+    imgPlayer[i] = loadImage(string1);       
   }
-  for (let j=1; j<=12; j++) {           
-    let string2 = "assets/cactus"+j+".png"; 
-    imgGroundObs[j]= loadImage(string2);       
+  for (let j=1; j<=12; j++) {                 // load images for ground obstacles
+    let string2 = "assets/cactus"+j+".png";   
+    imgGroundObs[j] = loadImage(string2);       
   }
-  for (let k=1; k<=2; k++) {           
+  for (let k=1; k<=2; k++) {                  // load images for air obstacles
     let string3 = "assets/bird"+k+".png"; 
-    imgAirObs[k]= loadImage(string3);       
+    imgAirObs[k] = loadImage(string3);       
   }
 
   imgBackground = loadImage("assets/bg.png");
@@ -50,12 +60,16 @@ function preload(){
   imgClouds = loadImage("assets/cloud.png");
 
   menuMusic = loadSound("assets/menu.mp3");
+  bgMusic = loadSound("assets/bg.mp3");
   jumpSound = loadSound("assets/jump.mp3");
   clickSound = loadSound("assets/click1.wav");
   gameOverSound = loadSound("assets/gameover.wav");
-
+  endMusic = loadSound("assets/end.mp3");
 }
 
+
+
+// creating setup and basic values
 function setup() {
   createCanvas(800, 400);
   lastAddedObstacle = 0;
@@ -73,10 +87,11 @@ function setup() {
 
   menuMusic.loop();
 
-
-
+  colours = true;
 }
 
+
+// main body of code, contains the "beginning", "middle", end" of the project
 function draw() {
   if (screen === "start") {
     startScreen();
@@ -89,7 +104,9 @@ function draw() {
   }
 }
 
-class Character{                  
+
+// creating classes for differect objects
+class Character{                       // player class
   constructor(){          
     this.w = 50;              
     this.h = 80;
@@ -100,10 +117,9 @@ class Character{
     this.img = imgPlayer[3];   
     this.w = this.img.width;      
     this.h = this.img.height;
-
   }  
   
-  jump(){  
+  jump(){                                // vertical displacement of dino
     if(this.y === height - this.h) {
       this.dy = -20;
     }                   
@@ -112,20 +128,22 @@ class Character{
   move(){                  
     this.y += this.dy;   
     this.dy += this.gravity;  
-    this.y = constrain(this.y, 0, height - this.h);                
+    this.y = constrain(this.y, 0, height - this.h);       // moving dinosaur in vertical direction, and limiting the y-value so it does not exceed the height of the screen         
   }
   
-  display(){                                       
-    image(this.img,this.x,this.y-25,this.w*1.5,this.h*1.5);         
+  display(){                             // displaying dino
+    setTint();                         
+    image(this.img,this.x,this.y-25,this.w*1.5,this.h*1.5);  
+    noTint();       
   }  
 
-  update(){                       
+  update(){                              // animation of dino
     if(CLD === 0){                               
       if(this.y < height-100){                  
         this.img = imgPlayer[2];                 
       }
       else {                                   
-        this.img = imgPlayer[imageIndex%2+3];        
+        this.img = imgPlayer[imageIndex%2+3];       // switching between dino image 3 and 4 to create animation
       } 
     }  
     else {                                      
@@ -133,13 +151,14 @@ class Character{
     }
   }
 
-  collide(Obs) {
+  collide(Obs) {                         // collision detection using p5js library
     return collideRectRect(this.x,this.y-25,this.w,this.h*0.65, Obs.x, Obs.y, Obs.w*0.75, Obs.h*0.55);
 
   }
 }
 
-class groundObstacles {
+
+class groundObstacles {         // ground obstacle (cactus) class
   constructor() {
     this.w = 50;
     this.h = 90;
@@ -160,7 +179,8 @@ class groundObstacles {
   }
 }
 
-class airObstacles {
+
+class airObstacles {           // air obstacle (bird) class
   constructor() {
     this.img = imgAirObs[1];
     this.w = this.img.width*1.6;
@@ -180,11 +200,12 @@ class airObstacles {
   }
 
   update() {
-    this.img = imgAirObs[imageIndex%2+1];
+    this.img = imgAirObs[imageIndex%2+1];   // switching between image 1 and 2 to create animation
   }
 }
 
-function pushObstacles() {
+
+function pushObstacles() {               // pushing/adding new obstacles
   let timePassed = random(700, 8000);           
   if (millis() - lastAddedObstacle > timePassed) { 
     if (int(timePassed)%2 === 0) {   
@@ -195,27 +216,30 @@ function pushObstacles() {
     }
     lastAddedObstacle = millis();              
   }
-
 }
+
+
 function keyPressed() {
-  if (keyCode === 87 || keyCode === 38) {
+  if (keyCode === 87 || keyCode === 38) {    // controlling the jump of the dino
     player.jump();
     jumpSound.play();
   }
-  else if (keyCode === 32) {
+  else if (keyCode === 32) {                 // restarting the game
     if (screen === "end" ) {
       window.location.reload();
     }
   }
 }
 
-function getScore() { 
-  beforeRoundScore = (millis() - startTime) /1000;
+
+function getScore() {                        // time gone by since starting the game
+  beforeRoundScore = (millis() - startTime) / 1000;
   score = beforeRoundScore.toFixed(3);
   return score;
 }
 
-function printScore() { 
+
+function printScore() {                      // displaying score on screen
   if (CLD === 0){
     fill("black");
     textSize(10);
@@ -225,7 +249,7 @@ function printScore() {
 }
 
 
-function displayBackground() {
+function displayBackground() {               // background display and animation (clouds, mountain)
   background(imgBackground);
   if (bcX + imgFloor.width/2 <= 0) {
     bcX = 0;
@@ -233,12 +257,12 @@ function displayBackground() {
   else {
     bcX -= 5;
   }
-  image(imgFloor, bcX, height-20, imgFloor.width, imgFloor.height);
+  image(imgFloor, bcX, height-20, imgFloor.width, imgFloor.height);       // if half of the image is off the screen set the image back to x-position 0
   image(imgClouds, bcX, 20, imgClouds.width, imgClouds.height);
-
 }
 
-function mousePressed() {
+
+function mousePressed() {                    // toggling game state, setting colour
   if (screen === "start") {
     if (mouseX > width/2-75 && mouseX < width/2+75 && mouseY > height-40-30 && mouseY < height-40+30) {
       screen = "game";
@@ -246,11 +270,38 @@ function mousePressed() {
       clickSound.setVolume(50);
       clickSound.play();
       menuMusic.stop();
-  
+      bgMusic.loop();
+    }
+    else if (mouseX > 540 && mouseX < 660 && mouseY > height-120 && mouseY < height - 80) {
+      colours = !colours;
+      clickSound.setVolume(50);
+      clickSound.play();
     }
   }
 }
 
+
+function setColour() {        // set colour of button
+  if (colours) {
+    fill("white");
+  }
+  else {
+    fill("blue");
+  }
+}
+
+
+function setTint() {          // set colour of dino
+  if (colours) {
+    noTint();
+  }
+  else {
+    tint("blue");
+  }
+}
+
+
+// creates the starting screen - with buttons and options
 function startScreen() {  
   background(96, 157, 255);
   image(imgPlayer[5], width/4, 100, imgPlayer[5].width, imgPlayer[5].height);
@@ -269,8 +320,58 @@ function startScreen() {
   textSize(30);                      
   text("Start", width/2, height-30);  
 
+
+  setColour();                        // set colour of button
+  rect(600, height-100, 60, 20);
+  fill(0);                 
+  textSize(10);                      
+  text("Colour", 600, height-96.5);  
 }
 
+
+// creates the main game screen with actions
+function gameScreen() {     
+  displayBackground();             // background
+
+  player.display();
+  player.move();
+
+  pushObstacles();                 // adding new obstacle
+  
+  if (frameCount%6 === 0) {
+    imageIndex++;
+  } 
+  for (let i of gObstacles) {      // collision detection for ground obstacles
+    i.move();                           
+    i.display();     
+    if(player.collide(i)){     
+      CLD = 1; 
+      getScore();
+      gameOverSound.play();
+      endMusic.loop();
+      bgMusic.stop();
+      screen = "end";
+    } 
+  }
+  for (let j of aObstacles) {      // collision detection for air obstacles
+    j.move();                           
+    j.display(); 
+    j.update();    
+    if(player.collide(j)) {     
+      CLD = 1; 
+      getScore();
+      gameOverSound.play();
+      endMusic.loop();
+      bgMusic.stop();
+      screen = "end"; 
+    }      
+  }
+  player.update();    // dino animation, changing which image in imgArray to display
+  printScore();
+}          
+
+
+// creates the ending screen with scores and restart button
 function endScreen() { 
   textAlign(CENTER);       
   textSize(70);             
@@ -280,61 +381,12 @@ function endScreen() {
   textSize(30);                         
   text("Score: "+score+" second(s)", width/2, height/2+32);      
 
-  fill(92,167,182);                     
+  fill(92, 167, 182);                     
   rectMode(CENTER);                      
   noStroke();                            
-  rect(width-60, 30, 200, 60, 5);     
-  fill(236,240,241);                      
+  rect(width - 60, 30, 200, 60, 5);     
+  fill(236, 240, 241);                      
   textSize(15);                           
   text("Press Space to Restart", width-80, 35);  
   noLoop();   
 }
-
-
-function gameScreen() {     
-  displayBackground();
-
-  player.display();
-  player.move();
-
-  pushObstacles();
-  
-  if (frameCount%6 === 0) {
-    imageIndex++;
-  } 
-  for (let i of gObstacles) {      
-    i.move();                           
-    i.display();     
-    if(player.collide(i)){     
-      CLD = 1; 
-      getScore();
-      gameOverSound.play();
-      screen = "end";
-    } 
-  }
-  
-  for (let j of aObstacles) {      
-    j.move();                           
-    j.display(); 
-    j.update();    
-    if(player.collide(j)){     
-      CLD = 1; 
-      getScore();
-      gameOverSound.play();
-      screen = "end"; 
-    }
-                        
-  }
-  player.update(); 
-  printScore();
-}          
- 
-
-
-// function setColor () {
-//   tint();
-//   noTint();
-//   right after you've set it 
-// }
-
-
