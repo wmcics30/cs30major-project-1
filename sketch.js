1779d32b4bfe0b6aca408a12bd09d7a11c8293c9
TacoPotato <77638119+TacoPotato@users.noreply.github.com>
2021-02-28 23:52:37 -0600

    Final Prototype-changes may be added later

--- sketch.js
+++ sketch.js
@@ -6,9 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let player, gObs, aObs;
-let lastAddedObstacle = 0;
-let gObstacles = []; 
-let aObstacles = [];               
+let lastAddedObstacle;
+let gObstacles; 
+let aObstacles;               
 let imgPlayer = [];
 let imgGroundObs = [];
 let imgAirObs = []; 
@@ -16,15 +16,15 @@ let imgAirObs = [];
 let imgBackground;
 let bcX;
 let imgFloor;
+let imgClouds;
       
 let jumpSound;   
 
-let CLD = 0;
-let imageIndex = 1;
+let CLD;
+let imageIndex;
 
-let score = 0;                
-let bestScore = 0;              
-let screen = 0;        
+let score;                           
+let screen;        
 
 function preload(){                     
   for (let i=1; i<=5; i++) {           
@@ -36,77 +36,45 @@ function preload(){
     imgGroundObs[j]= loadImage(string2);       
   }
   for (let k=1; k<=2; k++) {           
-    let string3 = "assets/parrots"+k+".png"; 
+    let string3 = "assets/bird"+k+".png"; 
     imgAirObs[k]= loadImage(string3);       
   }
 
   imgBackground = loadImage("assets/bg.png");
-  bcX = 0;
   imgFloor = loadImage("assets/base.png");
+  imgClouds = loadImage("assets/cloud.png");
 
 }
 
 function setup() {
   createCanvas(800, 400);
+  lastAddedObstacle = 0;
+  gObstacles = []; 
+  aObstacles = [];
   player = new Character();
   gObs = new groundObstacles();
-  // aObs = new airObstacles();
+  aObs = new airObstacles();
+
+  screen = "start";
+  bcX = 0;
+  score = 0;
+  imageIndex = 1;
+  CLD = 0;
+
+
 
 }
 
 function draw() {
-  background(imgBackground);
-  if (bcX + imgFloor.width/2 > 0) {
-    bcX -= 5;
-  }
-  else {
-    bcX = 0;
-  }
-  image(imgFloor, bcX, height-20, imgFloor.width, imgFloor.height);
-  
-
-  player.display();
-  player.move();
-  let timePassed = random(700, 8000);           
-  if (millis() - lastAddedObstacle > timePassed) {   
-    gObstacles.push(new groundObstacles());         
-    lastAddedObstacle = millis();              
-  }
-  
-  if (frameCount%6 === 0) {
-    imageIndex++;
+  if (screen === "start") {
+    startScreen();
   } 
-  for (let i of gObstacles) {      
-    i.move();                           
-    i.display();     
-    if(player.collide(i)){     
-      CLD = 1; 
-      textAlign(CENTER);       
-      textSize(70);             
-      text("Game Over",width/2, height/2);
-      noLoop();   
-      console.log(getScore());
-    } 
+  else if (screen === "game") {
+    gameScreen();
+  } 
+  else if (screen === "end") {
+    endScreen();
   }
-  // if (millis() - lastAddedObstacle > timePassed) {   
-  //   aObstacles.push(new airObstacles());         
-  //   lastAddedObstacle = millis();              
-  // }
-  // for (let j of aObstacles) {      
-  //   j.move();                           
-  //   j.display();     
-  //   if(player.collide(j)){     
-  //     CLD = 1; 
-  //     textAlign(CENTER);       
-  //     textSize(70);             
-  //     text("Game Over",width/2, height/2);
-  //     noLoop();   
-  //     console.log(getScore());
-  //   }
-                        
-  // }
-  player.update(); 
-  // aObs.update();
 }
 
 class Character{                  
@@ -176,41 +144,47 @@ class groundObstacles {
   }
 
   display() {
-    image(this.img,this.x,this.y-1.5,this.w,this.h); 
+    image(this.img,this.x,this.y-0.5,this.w,this.h); 
   }
 }
 
-// function setColor () {
-//   tint();
-//   noTint();
-//   right after you've set it 
-// }
+class airObstacles {
+  constructor() {
+    this.img = imgAirObs[1];
+    this.w = this.img.width*1.6;
+    this.h = this.img.height*1.6;
+    this.x = width;
+    this.y = random(2*height/3,5*height/6);
+    this.dx = -10;
 
-// class airObstacles {
-//   constructor() {
-//     this.img = imgAirObs[1];
-//     this.w = this.img.width*1.5;
-//     this.h = this.img.height*1.5;
-//     this.x = width;
-//     this.y = height/2;
-//     this.dx = -10;
+  }
 
-//   }
+  move() {
+    this.x = this.x + this.dx;
+  }
 
-//   move() {
-//     this.x = this.x-this.dx;
-//   }
+  display() {
+    image(this.img,this.x,this.y,this.w,this.h);
+  }
 
-//   display() {
-//     image(this.image,this.x,this.y,this.w,this.h);
-//   }
+  update() {
+    this.img = imgAirObs[imageIndex%2+1];
+  }
+}
 
-//   update() {
-//     this.img = imgAirObs[imageIndex%2+1];
-//   }
-// }
+function pushObstacles() {
+  let timePassed = random(700, 8000);           
+  if (millis() - lastAddedObstacle > timePassed) { 
+    if (int(timePassed)%2 === 0) {   
+      gObstacles.push(new groundObstacles());    
+    }
+    else {
+      aObstacles.push(new airObstacles());
+    }
+    lastAddedObstacle = millis();              
+  }
 
-
+}
 function keyPressed() {
   if (keyCode === 87 || keyCode === 38) {
     player.jump();
@@ -218,57 +192,130 @@ function keyPressed() {
 }
 
 function getScore() { 
-  if (CLD === 1) {
-    score = millis()/1000;
-    return score;
+  beforeRoundScore = millis()/1000;
+  score = beforeRoundScore.toFixed(3);
+  return score;
+}
+
+function printScore() { 
+  if (CLD === 0){
+    fill("black");
+    textSize(10);
+    textFont("Georgia");
+    text("Score:"+getScore(), 600, 20);
   }
- 
 }
 
 
-// function StartScreen() {             
-//  
-// 
-// }
+function displayBackground() {
+  background(imgBackground);
+  if (bcX + imgFloor.width/2 <= 0) {
+    bcX = 0;
+  }
+  else {
+    bcX -= 5;
+  }
+  image(imgFloor, bcX, height-20, imgFloor.width, imgFloor.height);
+  image(imgClouds, bcX, 20, imgClouds.width, imgClouds.height);
 
-// function endScreen() { 
-// 
-// }
+}
 
-
-// function gameScreen() {               
-//   background(0);             
-//   player.display();
-//   player.move();
-//   let timePassed = random(700, 8000);           
-//   if (millis() - lastAddedObstacle > timePassed) {   
-//       gObstacles.push(new groundObstacles());         
-//       lastAddedObstacle = millis();              
-//   }
-//   if (frameCount%6===0) imageIndex++; 
-//     for (let i of gObstacles) {      
-//         i.move();                           
-//         i.display();     
-//         if(player.collide(i)){     
-//           CLD = 1; 
-//           textAlign(CENTER);       
-//           textSize(70);             
-//           text("Game Over",width/2, height/2);
-//           noLoop();   
-//         }
-                          
-//     }
+function mousePressed() {
+  if (screen = "start") {
+    if (mouseX > width/2-75 && mouseX < width/2+75 && mouseY > height-40-30 && mouseY < height-40+30) {
+      screen = "game";
+    }
+  }
+  else if (screen = "end") {
+    if (mouseX > width-50-100 && mouseX < width-50+100 && mouseY > 0 && mouseY < 30+60) {
+      screen = "start";
+      setup();
+    }
+  }
   
-//   player.update();                        
+}
 
+function startScreen() {  
+  background(96, 157, 255);
+  image(imgPlayer[5], width/4, 100, imgPlayer[5].width, imgPlayer[5].height);
+  fill(255);
+  textAlign(CENTER);
+  textSize(50);
+  fill(52, 73, 94);  
+  textFont("Georgia")                
+  text("Jumping Dino", width / 2, height / 2);
+
+  fill(92,167,182);                   
+  noStroke();                         
+  rectMode(CENTER);                  
+  rect(width/2, height-40, 150, 60); 
+  fill(236,240,241);                 
+  textSize(30);                      
+  text("Start", width/2, height-30);  
+
+}
+
+function endScreen() { 
+  background(23, 24, 24,3); 
+  textAlign(CENTER);       
+  textSize(70);             
+  text("Game Over",width/2, height/2);
+
+  fill(230, 180, 80);                    
+  textSize(30);                         
+  text("Score: "+getScore()+" second(s)", width/2, height/2+32);      
+
+  fill(92,167,182);                     
+  rectMode(CENTER);                      
+  noStroke();                            
+  rect(width-50, 30, 200, 60, 5);     
+  fill(236,240,241);                      
+  textSize(30);                           
+  text("Restart", width-70, 38);     
+
+}
+
+
+function gameScreen() {     
+  displayBackground();
+
+  player.display();
+  player.move();
+
+  pushObstacles();
+  
+  if (frameCount%6 === 0) {
+    imageIndex++;
+  } 
+  for (let i of gObstacles) {      
+    i.move();                           
+    i.display();     
+    if(player.collide(i)){     
+      CLD = 1; 
+      screen = "end";
+    } 
+  }
+  
+  for (let j of aObstacles) {      
+    j.move();                           
+    j.display(); 
+    j.update();    
+    if(player.collide(j)){     
+      CLD = 1; 
+      screen = "end"; 
+    }
+                        
+  }
+  player.update(); 
+  printScore();
+}          
+ 
+
+
+// function setColor () {
+//   tint();
+//   noTint();
+//   right after you've set it 
 // }
-
-// function endScreen() { 
-// }
-
-// function printScore() { 
-// }
-
-
 
 
